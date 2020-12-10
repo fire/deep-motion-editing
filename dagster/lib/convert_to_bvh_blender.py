51292749da07846d63a1b9881b6149400189b10e
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-12-10 14:27:11 -0800

    Use root transform as translation only.

--- dagster/lib/convert_to_bvh_blender.py
+++ dagster/lib/convert_to_bvh_blender.py
@@ -8,7 +8,7 @@ bpy.ops.import_scene.gltf(filepath=path)
 armature_count = 0
 for i in range(len(bpy.context.scene.objects)):
     element = bpy.context.scene.objects[i]
-    if element.type != "ARMATURE": 
+    if element.type != "ARMATURE":
         continue
     armature_count += 1
 
@@ -17,8 +17,9 @@ if armature_count > 1:
 
 for i in range(len(bpy.context.scene.objects)):
     element = bpy.context.scene.objects[i]
-    if element.type != "ARMATURE": 
+    if element.type != "ARMATURE":
         continue
-    bpy.ops.export_anim.bvh(filepath=f'{path}.bvh', check_existing=True, filter_glob="*.bvh",
-        global_scale=1, frame_start=0, frame_end=0, rotate_mode='NATIVE', root_transform_only=False)
+    bpy.ops.export_anim.bvh(filepath=f'{path}.bvh',
+                            check_existing=True, filter_glob="*.bvh",
+                            global_scale=1, frame_start=0, frame_end=0, rotate_mode='NATIVE', root_transform_only=True)
     break
