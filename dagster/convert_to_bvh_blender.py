4db276790063bd0789711657b713020fd942754c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-12-10 09:25:57 -0800

    Update logging.

--- dagster/convert_to_bvh_blender.py
+++ dagster/convert_to_bvh_blender.py
@@ -9,5 +9,6 @@ for i in range(len(bpy.context.scene.objects)):
     element = bpy.context.scene.objects[i]
     if element.type != "ARMATURE": 
         continue
-    bpy.ops.export_anim.bvh(filepath=f'{path}.{i}.bvh', check_existing=True, filter_glob="*.bvh", 
-global_scale=1, frame_start=0, frame_end=0, rotate_mode='NATIVE', root_transform_only=False)
+    bpy.ops.export_anim.bvh(filepath=f'{path}.bvh', check_existing=True, filter_glob="*.bvh",
+        global_scale=1, frame_start=0, frame_end=0, rotate_mode='NATIVE', root_transform_only=False)
+    break
