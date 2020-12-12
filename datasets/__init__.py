24acaa01b19fa9e9598588fa13dfb055cbc3abbc
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-12-12 11:59:17 -0800

    Divide into two groups.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,18 +3,18 @@ def get_character_names(args):
                "BerkeleyMHAD_skl_s02",
                "BerkeleyMHAD_skl_s03",
                "BerkeleyMHAD_skl_s04",
-               "BerkeleyMHAD_skl_s05",
+               ]
+    group_b = ["BerkeleyMHAD_skl_s05",
                "BerkeleyMHAD_skl_s06",
                "BerkeleyMHAD_skl_s07",
-               "BerkeleyMHAD_skl_s08",
-               ]
+               "BerkeleyMHAD_skl_s08",]
     if args.is_train:
         """
         Put the name of subdirectory in retargeting/datasets/Mixamo as [[names of group A], [names of group B]]
         """
         characters = [
             group_a,
-            group_a
+            group_b
         ]
 
     else:
@@ -27,8 +27,8 @@ def get_character_names(args):
             "BerkeleyMHAD_skl_s10",
             ],
             [
-                "BerkeleyMHAD_skl_s09",
-                "BerkeleyMHAD_skl_s10",
+                "BerkeleyMHAD_skl_s07",
+                "BerkeleyMHAD_skl_s08",
             ],
         ]
         tmp = characters[1][args.eval_seq]
--- eval_single_pair.py
+++ eval_single_pair.py
@@ -43,7 +43,7 @@ def main():
 
     dataset = create_dataset(args, character_names)
     model = create_model(args, character_names, dataset)
-    model.load(epoch=8600)
+    model.load(epoch=0)
     input_motion = []
 
     if not os.path.exists(input_bvh):
