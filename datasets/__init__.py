ca856c89719c6040e5cbde693fa5eec7c5dd09b2
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-12-14 11:36:54 -0800

    Try move animations to the same rig.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -22,7 +22,7 @@ def get_character_names(args):
         Put the name of subdirectory in retargeting/datasets/Mixamo as [[names of group A], [names of group B]]
         """
         characters = [
-            group_c,
+            group_d,
             group_d,
         ]
 
--- datasets/combined_motion.py
+++ datasets/combined_motion.py
@@ -119,20 +119,10 @@ class MixedData(Dataset):
     def __len__(self):
         return self.length
 
-     
-    def get_item(self, gid, pid, id):
-        character = self.characters[gid][pid]
-        path = './datasets/Mixamo/{}/'.format(character)
-        if isinstance(id, int):
-            file = path + self.file_list[id]
-        elif isinstance(id, str):
-            file = id
-        else:
-            raise Exception('Wrong input file type')
-        if not os.path.exists(file):
-            raise Exception('Cannot find file')
-        file = BVH_file(file)
-        motion = file.to_tensor(quater=self.args.rotation == 'quaternion')
+
+    def get_item_string(self, item_string):
+        item_file = BVH_file(item_string)
+        motion = item_file.to_tensor(quater=self.args.rotation == 'quaternion')
         motion = motion[:, ::2]
         length = motion.shape[-1]
         length = length // 4 * 4
@@ -221,19 +211,9 @@ class TestData(Dataset):
         return res
 
  
-    def get_item(self, gid, pid, id):
-        character = self.characters[gid][pid]
-        path = './datasets/Mixamo/{}/'.format(character)
-        if isinstance(id, int):
-            file = path + self.file_list[id]
-        elif isinstance(id, str):
-            file = id
-        else:
-            raise Exception('Wrong input file type')
-        if not os.path.exists(file):
-            raise Exception('Cannot find file')
-        file = BVH_file(file)
-        motion = file.to_tensor(quater=self.args.rotation == 'quaternion')
+    def get_item_string(self, item_string):
+        item_file = BVH_file(item_string)
+        motion = item_file.to_tensor(quater=self.args.rotation == 'quaternion')
         motion = motion[:, ::2]
         length = motion.shape[-1]
         length = length // 4 * 4
